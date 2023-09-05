75ce6a500196f3633bac3e95f91a8275fdbb3421
SSchmerek <s.schmerek@gmail.com>
2023-09-05 21:29:32 +0200

    try fix global not found

--- webapp/src/lib/server/prisma.ts
+++ webapp/src/lib/server/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
 // @ts-ignore
-const prisma = global.prisma || new PrismaClient();
+const prisma = global?.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") {
     // @ts-ignore
