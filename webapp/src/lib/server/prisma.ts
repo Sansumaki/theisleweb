9eeb66a775288803b6683a8015528b1d6021e380
SSchmerek <s.schmerek@gmail.com>
2023-09-05 21:45:11 +0200

    remove global access

--- webapp/src/lib/server/prisma.ts
+++ webapp/src/lib/server/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-// @ts-ignore
-const prisma = global?.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") {
-    // @ts-ignore
-    global.prisma = prisma;
-}
+const prisma = new PrismaClient();
+// Cloudflare can't handle global (global is not defined)
+// const prisma = global?.prisma || new PrismaClient();
+//
+// if (process.env.NODE_ENV === "development" && global) {
+//     global.prisma = prisma;
+// }
 
 export { prisma };
