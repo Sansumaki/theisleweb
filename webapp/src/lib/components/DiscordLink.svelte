4e56bde0e2e89b090bd6bf5ccf26f631f6f3253a
san <s.schmerek@gmail.com>
2023-10-19 16:46:49 +0200

    change discord invite link

--- webapp/src/lib/components/DiscordLink.svelte
+++ webapp/src/lib/components/DiscordLink.svelte
@@ -4,7 +4,7 @@
     import {t} from "$lib/translations";
 </script>
 
-<A href="https://discord.gg/ftr" target="_blank"
+<A href="https://join.ftr.cool" target="_blank"
    aClass="inline-flex justify-center items-center p-5 mb-4 text-base font-medium rounded-lg bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600">
     <span class="w-full ml-3">{$t("common.joinDiscord")}</span>
     <Icon name="arrow-right-outline" class="w-4 h-4 ml-2"/>
