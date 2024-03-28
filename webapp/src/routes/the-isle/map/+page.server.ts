5060bbd3fc7436b6974f04b04fd09c6a2ea3086d
SSchmerek <s.schmerek@gmail.com>
2024-03-28 21:58:51 +0100

    update packages and some logs

--- webapp/src/routes/the-isle/map/+page.server.ts
+++ webapp/src/routes/the-isle/map/+page.server.ts
@@ -14,7 +14,11 @@ export const load = (async ({ cookies, platform }) => {
 		const showPoi = cookies.get(showPoiPath) === 'true';
 		const selectedMapKey = cookies.get(selectedMapKeyPath);
 
-		if (platform?.env == undefined) return;
+		console.log(platform)
+		console.log(platform?.env)
+		if (platform?.env == undefined) {
+			 throw ("platform.env is not defined!")
+		}
 		const pandasiaDB = new PandasiaDatabase(platform.env.DB);
 
 		const mapList = await pandasiaDB.getMapList();
